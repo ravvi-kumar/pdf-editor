691b53dc759f96630afd7ba4008403539e0523ca
ravvi-kumar <ravvi.kumar8989@gmail.com>
2025-07-27 19:36:45 +0530

    db setup and upload thing setup

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,11 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
-});
+export const pdfFile = pgTable("pdf_file", {
+	id: uuid('id').defaultRandom().primaryKey(),
+	content: text('content'),
+	fileName: text('file_name').notNull(),
+	fileUrl: text('file_url').notNull(),
+
+	updated_at: timestamp(),
+	created_at: timestamp().defaultNow().notNull(),
+})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { createUploader } from '$lib/utils/uploadthing';
+
+	import { UploadButton, UploadDropzone } from '@uploadthing/svelte';
+
+	const uploader = createUploader('imageUploader', {
+		onClientUploadComplete: (res) => {
+			console.log(`onClientUploadComplete`, res);
+			alert('Upload Completed');
+		},
+		onUploadError: (error: Error) => {
+			alert(`ERROR! ${error.message}`);
+		}
+	});
+</script>
+
+<div class="p-4">
+	<h1 class="text-2xl font-bold">Pdf Editor</h1>
+	<div class="mx-auto max-w-md">
+		<div class="ut-flex ut-flex-col ut-items-center ut-justify-center ut-gap-4">
+			<UploadDropzone {uploader} />
+		</div>
+	</div>
+</div>
